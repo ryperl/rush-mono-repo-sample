89554169981d1314f09f365eab929d620a0e8b14
Ryan Perlman <ryperl@live.com>
2020-12-04 02:30:25 -0800

    Rush Mono Repo sample

---
+++ apps/landing/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Thing } from '@shared/components';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Thing />
+        <span>Custom Template</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
